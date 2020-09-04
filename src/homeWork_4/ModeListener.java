51b012b017b237b1ec6493a58dff593d14d4cfcc
Vadim Balakirev <it-balakirev@mail.ru>
2020-09-04 15:22:12 +0300

    #homeWork4 added.

---
+++ src/homeWork_4/ModeListener.java
@@ -0,0 +1,14 @@
+package homeWork_4;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ModeListener implements ActionListener {
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
